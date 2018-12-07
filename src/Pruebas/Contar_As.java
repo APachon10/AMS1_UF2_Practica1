df7fd0ec1ffbb9af724474c8187a0a07d855febf
xp <xp@xp-PC>
2018-12-07 01:23:12 +0100

    Contar AS:Logica del Programa Hecho,pero se necesita encontrar una formade almacenar la anterior para compararla

--- src/Pruebas/Contar_As.java
+++ src/Pruebas/Contar_As.java
@@ -1,5 +1,39 @@
 package Pruebas;
 
-public class Contar_As {
+import java.util.Scanner;
 
+public class Contar_As {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String cadena = "";
+		boolean salir = false;
+		
+		do {
+			System.out.println("Introduce una cadena ");
+			cadena = sc.nextLine();
+			if(cadena.equalsIgnoreCase("fi")) {
+				salir = true;
+				System.out.println("Programa Terminado ");
+			}else {
+				contar_As(cadena);
+			}
+			
+		}while(!salir);
+	}
+	public static void contar_As(String cadena){
+		String cadena_auxiliar = "" ;
+		int cont1=0,cont_auxiliar = 0;
+		
+		System.out.println("cadena anterior --> "+cadena_auxiliar);
+		System.out.println("====================================");
+		System.out.println("cadena recibida --> "+cadena);
+		System.out.println("====================================");
+		
+		
+		for (int i = 0; i < cadena.length(); i++) {
+			if(cadena.charAt(i) == 'a') {
+				cont1++;
+			}
+		}
+	}
 }
