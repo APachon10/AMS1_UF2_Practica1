85e7941a8a0c3d5f9bcee85b415627f4e7218acd
xp <xp@xp-PC>
2018-12-09 00:29:14 +0100

    Intro_Array : Introduccion de valores implementada parcialmente falta hacer que se ejecute mas de una vez

--- src/Pruebas/IntroArray.java
+++ src/Pruebas/IntroArray.java
@@ -9,25 +9,28 @@ public class IntroArray {
 		IntroArray r = new IntroArray();
 		int array_prueba []=  new int[10];
 		String notas = "";
-		String confirmacion = "";
 		String [] notas2 = null;
 		boolean salir = false;
 		int i=0;
 		do {
 			System.out.println("Introduce las notas hasta que pongas -1");
 			notas = scan.nextLine();
-			for(i=0;i<notas.length();i++) {
-				System.out.print("Cadena --> " + notas);
-				notas2[i]= notas.split(" ");
+			notas2= notas.split(" ");
+			for(i=0;i<notas2.length;i++) {
+				System.out.println("Cadena --> " + notas2[i]);
 				if(notas2[i].equals("-1")) {
-					
+					salir=true;
+				}else {
+					r.leerNotas(array_prueba, notas);
 				}
 			}
 		}while(!salir);
+		System.out.println("=========================");
 		//Mostramos el Array
 		System.out.println("Vector Notas --> "+Arrays.toString(array_prueba));
 	}
 	public void leerNotas(int array_prueba [],String notas) {
+		System.out.println("Leyendo y insertando notas ");
 		String notas2 []= notas.split(" "); 
 		for (int j = 0; j < notas2.length; j++) {
 			System.out.println("Notas 2 --> "  + notas2[j]);
@@ -45,5 +48,23 @@ public class IntroArray {
 			}	
 		}
 	}
+	/*public void Menu() {
+		Scanner scan = new Scanner(System.in);
+		int op=0;
 
+		do {
+			System.out.println("Bienvenido que operacion desea realizar ? "
+					+ "\n1- Añadir Notas"
+					+ "\n2- Salir ");
+			op = scan.nextInt();
+
+			switch (op) {
+			case 1:
+				leerNotas(array_prueba, notas);
+				break;
+
+			default:
+				break;
+			}
+		}while(op!=2);*/
 }
