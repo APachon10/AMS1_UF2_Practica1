522f46edf3555c61fd1b0a460c95217559c9d5af
xp <xp@xp-PC>
2018-12-09 18:04:06 +0100

    Que queda por implementar ? Solucionar Entrada Datos en IntroArray

--- src/Pruebas/IntroArray.java
+++ src/Pruebas/IntroArray.java
@@ -9,19 +9,28 @@ public class IntroArray {
 		int array_prueba []=  new int[10];
 		String notas = "";
 		r.Menu(array_prueba,notas);
-		
+
 	}
 	public void IntrNotas(int array_prueba [],String notas) {
-		String notas2 []= notas.split(" "); 
+		String notas2 []= notas.split(" ");
+		int i=0;
 		for (int j = 0; j < notas2.length; j++) {
+			System.out.println("Notas2 --> "+notas2[j]);
+			System.out.println("=========================");
 			if(!notas2[j].equals("-1")) {
 				if(array_prueba[j] != 0) {
-					j++;
+					System.out.println("Entramos dentro del Array y vemos la pos  --> "+array_prueba[j]);
+					System.out.println("=========================");
 				}else {
 					int conver = Integer.parseInt(notas2[j]);
+					System.out.println("Cadena convertida --> "+conver);
+					System.out.println("=========================");
 					array_prueba[j] = conver;
+					System.out.println("Array_Prueba --> "+array_prueba[j]);
+					System.out.println("=========================");
 				}
 			}
+
 		}
 	}
 	public void Menu(int array_prueba [],String notas) {
